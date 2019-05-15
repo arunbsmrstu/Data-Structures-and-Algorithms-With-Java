628f6186df052ae6027f0bb18f08e075b7407a24
arun <arunbsmrstu@gmail.com>
2019-05-15 18:12:58 +0600

    first commit

---
+++ src/datastructures_/_algorithms/DataStructures__Algorithms.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructures_._algorithms;
+
+/**
+ *
+ * @author Arun
+ */
+public class DataStructures__Algorithms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
