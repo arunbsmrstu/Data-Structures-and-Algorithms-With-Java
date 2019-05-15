2acb3cd23b33e9a0f761075e1cf21b8fbf8f000e
unknown <arunbsmrstu@gmail.com>
2019-05-16 02:25:15 +0600

    selection sort

---
+++ src/com/algorithms/sorting/Selection.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.algorithms.sorting;
+
+import java.util.List;
+import jdk.nashorn.internal.parser.TokenType;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Selection {
+
+    public static void sort(int[] a) {
+        int N = a.length; // array length
+        for (int i = 0; i < N; i++) { // Exchange a[i] with smallest entry in a[i+1...N).
+            int min = i; // index of minimal entr.
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
+    }
+    
+    private static boolean less(int v, int w) {
+        if (v == w) {
+            return false;
+        } else if (v > w) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private static void exch(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void show(int[] a) {
+        for(int i=0;i<a.length;i++)
+            System.out.print(a[i]+" ");
+
+    }
+
+    private static boolean isSorted(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+
+}
--- src/datastructures_/_algorithms/DataStructures__Algorithms.java
+++ src/datastructures_/_algorithms/DataStructures__Algorithms.java
@@ -5,6 +5,13 @@
  */
 package datastructures_._algorithms;
 
+import com.algorithms.sorting.Selection;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author Arun
@@ -14,8 +21,20 @@ public class DataStructures__Algorithms {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException {
+        File file= new File("Selection.txt");
+        Scanner sc= new Scanner(file);
+        List<Integer> list=new ArrayList<>();
+        int [] a= new int[16];
+        int count=0;
+        while(sc.hasNextInt()){
+            a[count]=sc.nextInt();
+            count++;
+        }
+        
+        Selection.sort(a);
+        Selection.show(a);
+        
     }
     
 }
