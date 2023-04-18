6a7d7bc52d0b39b9e61c6d35b5e5cbab9b0df460
Arun <arunbsmrstu@gmail.com>
2023-04-18 22:39:23 +0600

    update

--- DS_ALGO/src/com/ds/algorithm/bst/BinarySearchTree.java
+++ DS_ALGO/src/com/ds/algorithm/bst/BinarySearchTree.java
@@ -1,5 +1,50 @@
 package com.ds.algorithm.bst;
 
 public class BinarySearchTree {
-    public BinaryNode binaryNode;
+    public BinaryNode root;
+
+    //insert value in a binary tree
+
+    private BinaryNode insert(BinaryNode binaryNode,int value){
+        if(binaryNode==null){
+            BinaryNode newNode= new BinaryNode();
+            newNode.value=value;
+            System.out.println("Value is successfully inserted !");
+            return newNode;
+        }else if(value<binaryNode.value){
+            binaryNode.left=insert(binaryNode.left,value);
+            return binaryNode;
+        }else{
+            binaryNode.right=insert(binaryNode.right,value);
+            return binaryNode;
+        }
+    }
+
+    public void insert(int value){
+        root=insert(root,value);
+    }
+
+    public void preOrderTravers(BinaryNode binaryNode){
+        if(binaryNode==null)
+            return;
+        System.out.print(binaryNode.value +" ->");
+        preOrderTravers(binaryNode.left);
+        preOrderTravers(binaryNode.right);
+    }
+
+    public void inOrderTravers(BinaryNode binaryNode){
+        if(binaryNode==null)
+            return;
+        inOrderTravers(binaryNode.left);
+        System.out.print(binaryNode.value +" ->");
+        inOrderTravers(binaryNode.right);
+    }
+
+    public void postOrderTravers(BinaryNode binaryNode){
+        if(binaryNode==null)
+            return;
+        postOrderTravers(binaryNode.left);
+        postOrderTravers(binaryNode.right);
+        System.out.print(binaryNode.value +" ->");
+    }
 }
--- DS_ALGO/src/com/ds/algorithm/bst/Main.java
+++ DS_ALGO/src/com/ds/algorithm/bst/Main.java
@@ -3,5 +3,20 @@ package com.ds.algorithm.bst;
 public class Main {
     public static void main(String arg[]){
         BinarySearchTree binarySearchTree= new BinarySearchTree();
+        binarySearchTree.insert(70);
+        binarySearchTree.insert(50);
+        binarySearchTree.insert(90);
+        binarySearchTree.insert(30);
+        binarySearchTree.insert(60);
+        binarySearchTree.insert(80);
+        binarySearchTree.insert(100);
+        binarySearchTree.insert(20);
+        binarySearchTree.insert(40);
+        System.out.println("\nPre Order Travers");
+        binarySearchTree.preOrderTravers(binarySearchTree.root);
+        System.out.println("\nIn Order Travers");
+        binarySearchTree.inOrderTravers(binarySearchTree.root);
+        System.out.println("\nPost Order Travers");
+        binarySearchTree.postOrderTravers(binarySearchTree.root);
     }
 }
